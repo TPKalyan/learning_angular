e202b76b196c92403a18060b20a74329e6955a17
Pawan kalyan <pavankalyanthota@gmail.com>
2018-06-26 17:50:44 +0530

    added feature to show weather a topping is added or not

--- Mod3Lab/app/pizzaController.js
+++ Mod3Lab/app/pizzaController.js
@@ -1,6 +1,7 @@
 app.controller('pizzaController', [
   '$scope',
   function($scope) {
+    $scope.toppingAdded=false;
     $scope.model = {
       title: 'Pizza Builder',
       availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
@@ -9,6 +10,15 @@ app.controller('pizzaController', [
     $scope.addTopping = function(topping){
       $scope.model.selectedToppings.push(topping);
       $scope.model.search = null;
+      $scope.toppingAdded = true;
+    }
+    $scope.reset = function(){
+      $scope.toppingAdded=false;
+    }
+    $scope.filteredToppings = function(searchString){
+      return $scope.model.availableToppings.filter((topping)=>{
+        return topping.includes(searchString)
+      })
     }
   }
 ]);
