c81d6b8de5af62581b3582f307d5bf978642d982
Pawan kalyan <pavankalyanthota@gmail.com>
2018-06-27 12:40:41 +0530

    implemented asynchronus method to check weather a number is odd or not

--- Mod4Lab/app/labController.js
+++ Mod4Lab/app/labController.js
@@ -1,5 +1,36 @@
 app.controller('labController', [
-    '$scope',
-    function ($scope) {
+    '$scope','$timeout','$q',
+    function ($scope,$timeout,$q) {
+      $scope.model = {
+        number:0,
+        result:"Ready"
+      }
+
+      $scope.checkOddNumber = function(number){
+        $scope.model.result = 'Working...';
+        checkOddNumberHandler(number).then(function(result){
+          $scope.model.result = 'Success: ' + result;
+        },
+        function(result){
+          $scope.model.result = 'Error: ' + result;
+        });
+      }
+
+      function checkOddNumberHandler(number){
+        var defer = $q.defer();
+
+        $timeout(function(){
+          if (isNumberOdd(number)) {
+            defer.resolve('Yes,'+number+' is odd');
+          }else{
+            defer.reject('No,'+number+' is not odd');
+          }
+        },1000);
+        return defer.promise;
+      }
+
+      let isNumberOdd = (number)=>{
+        return !isNaN(number) && number%2 == 1;
+      }
     }
 ]);
