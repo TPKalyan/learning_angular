d65c4db5073359d15bddb081a9a10c5db9c7d472
Pawan kalyan <pavankalyanthota@gmail.com>
2018-06-27 12:54:43 +0530

    refactored app.js in Mod4

--- Mod4Lab/app/labController.js
+++ Mod4Lab/app/labController.js
@@ -1,24 +1,22 @@
 app.controller('labController', [
     '$scope','$timeout','$q',
     function ($scope,$timeout,$q) {
+
       $scope.model = {
         number:0,
         result:"Ready"
       }
 
-      $scope.checkOddNumber = function(number){
-        $scope.model.result = 'Working...';
-        checkOddNumberHandler(number).then(function(result){
-          $scope.model.result = 'Success: ' + result;
-        },
-        function(result){
-          $scope.model.result = 'Error: ' + result;
-        });
+      let isNumberOdd = (number)=>{
+        return !isNaN(number) && number%2 == 1;
       }
 
-      function checkOddNumberHandler(number){
-        var defer = $q.defer();
+      let showMessage = (message)=>{
+        $scope.model.result = message;
+      };
 
+      let checkOddNumberHandler = (number)=>{
+        var defer = $q.defer();
         $timeout(function(){
           if (isNumberOdd(number)) {
             defer.resolve('Yes,'+number+' is odd');
@@ -29,8 +27,17 @@ app.controller('labController', [
         return defer.promise;
       }
 
-      let isNumberOdd = (number)=>{
-        return !isNaN(number) && number%2 == 1;
+      let checkOddNumber = function(number){
+        $scope.model.result = 'Working...';
+        checkOddNumberHandler(number).then(function(result){
+          showMessage('Success: ' + result);
+        },
+        function(result){
+          showMessage('Error: ' + result);
+        });
       }
+
+
+      $scope.checkOddNumber = checkOddNumber;
     }
 ]);
