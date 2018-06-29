7b1804b772c78fd78d7c434184fe13a27fe10640
Pawan kalyan <pavankalyanthota@gmail.com>
2018-06-29 11:59:15 +0530

    added added github service

---
+++ Mod4Lab/usingHttp/app/gitHubService.js
@@ -0,0 +1,23 @@
+var gitHubService = angular.module('gitHubService', ['ngResource']);
+
+gitHubService.factory('gitHub', [
+  '$resource',
+  function ($resource) {
+    return $resource('https://api.github.com/:action/angular/:id',
+    {
+      action: '@action',
+      id: '@id'
+    },
+    {
+      getAll: {
+        method: 'GET',
+        isArray: true,
+        params: { action: 'orgs', id: 'repos' }
+      },
+      getDetail: {
+        method: 'GET',
+        params: { action: 'repos' }
+      },
+    });
+  }
+]);
